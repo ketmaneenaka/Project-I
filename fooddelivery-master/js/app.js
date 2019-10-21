6a7873a8c5ea305397195c72d6a1b16416d8322e
เก็จมณี <s6030213002@phuket.psu.ac.th>
2019-10-21 09:22:10 +0700

    category complete

--- fooddelivery-master/js/app.js
+++ fooddelivery-master/js/app.js
@@ -43,28 +43,53 @@ document.addEventListener('init', function (event) {
   if (page.id === 'homePage') {
     console.log("homePage");
 
+    
+
 //fastfood
-     $("#btn1").click(function () {
-      window.location='fastfood.html'  
-       console.log("go");
-     });
+    // $("#btn1").click(function () {
+      //window.location='fastfood.html'  
+       //console.log("go");
+     //});
 //dessert
-$("#btn2").click(function () {
-  window.location='dessert.html'  
-   console.log("go");
- });
+//$("#btn2").click(function () {
+  //window.location='dessert.html'  
+   //console.log("go");
+// });
 
 //Drink
-$("#btn3").click(function () {
-  window.location='drink.html'  
-   console.log("go");
- });
+//$("#btn3").click(function () {
+  //window.location='drink.html'  
+   //console.log("go");
+ //});
 
 //Islam
-$("#btn4").click(function () {
-  window.location='islam.html'  
-   console.log("go");
- });
+//$("#btn4").click(function () {
+  //window.location='islam.html'  
+   //console.log("go");
+ //});
+
+ $("#fastfoodbtn").click(function () {
+  localStorage.setItem("selectedCategory", "fastfood");
+  $("#content")[0].load("category.html");
+});
+
+$("#dessertbtn").click(function () {
+  localStorage.setItem("selectedCategory", "dessert");
+  $("#content")[0].load("category.html");
+});
+
+$("#drinkbtn").click(function () {
+  localStorage.setItem("selectedCategory", "drink");
+  $("#content")[0].load("category.html");
+});
+
+$("#islambtn").click(function () {
+  localStorage.setItem("selectedCategory", "islam");
+  $("#content")[0].load("category.html");
+});
+
+
+
 
 
     $("#menubtn").click(function () {
@@ -117,6 +142,39 @@ $("#btn4").click(function () {
       $("#sidemenu")[0].close();   
     });
   }
+
+  if (page.id === 'categoryPage') {
+    var category = localStorage.getItem("selectedCategory");
+    console.log("categoryPage:" + category);
+
+    $("#header").html(category);
+
+    $("#menubtn").click(function () {
+      $("#sidemenu")[0].open();
+    });
+
+    $("#list").empty();
+    db.collection("recommended").where("category", "==", category).get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        var item = `<ons-row class="category">
+                <ons-col modifier="nodivider">
+                    <div class="category_header" style="background-image: url('${doc.data().photoUrl}');background-size: 100%; ">
+                        <figure class="category_thumbnail" id="thaibtn">
+                            <div class="category_title" id="Category_1_name">${doc.data().name}</div>
+                        </figure>
+                    </div>
+                </ons-col>
+         </ons-row>`
+        $("#list").append(item);
+        console.log(doc.data().name);
+        
+      });
+    });
+
+  }
+
+
 
   if (page.id === 'loginPage') {
     console.log("loginPage");
